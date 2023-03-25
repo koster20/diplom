61c11a8764e9a72dbb008c724f2362d87fd8954d
koster20 <koster20011@gmail.com>
2023-03-25 20:08:41 +0400

    Fixed methods

--- generator/generator.go
+++ generator/generator.go
@@ -10,7 +10,7 @@ type pointFromMethods struct {
 	trapezoidPoint float64
 }
 
-const eps = 0.001
+const eps = 0.01
 
 func Base_function(x float64) float64 {
 	return x*x - math.Pi*math.Pi
@@ -18,8 +18,8 @@ func Base_function(x float64) float64 {
 
 func integration_core(number_of_terms int, point float64, static_point float64) float64 {
 	sum := 0.0
-	for i := 0; i < number_of_terms; i++ {
-		sum += math.Cos(point - static_point)
+	for i := 1; i <= number_of_terms; i++ {
+		sum += math.Cos(float64(i) * (point - static_point))
 	}
 
 	return (1.0 / math.Pi) * (0.5 + sum)
@@ -29,11 +29,11 @@ func trapezoid_rule(left float64, right float64, number_of_steps int, number_of_
 	sum := float64(0)
 	h := (right - left) / float64(number_of_steps)
 	for i := 1; i < number_of_steps; i++ {
-		sum += Base_function(left + float64(i)*h)
+		sum += integration_core(number_of_terms, left+float64(i)*h, point_of_research) * Base_function(left+float64(i)*h)
 	}
 	sum += integration_core(number_of_terms, left, point_of_research)*Base_function(left) + integration_core(number_of_terms, right, point_of_research)*Base_function(right)/2.0
 	sum *= h
-	fmt.Println(sum)
+
 	ch <- sum
 }
 
@@ -53,7 +53,7 @@ func simpsonRule(left float64, right float64, number_of_steps int, number_of_ter
 	}
 	sum = sum + integration_core(number_of_terms, left, point_of_research)*Base_function(left) + integration_core(number_of_terms, right, point_of_research)*Base_function(right)
 	sum = sum * (h / 3)
-	fmt.Println(sum)
+
 	ch <- sum
 }
 
@@ -63,17 +63,18 @@ func RungeSimp(point float64, number_of_terms int) float64 {
 	ch := make(chan float64, 2)
 	go simpsonRule(-math.Pi, math.Pi, 5, number_of_terms, point, ch)
 	step = <-ch
-	go simpsonRule(-math.Pi, math.Pi, 5, number_of_terms, point, ch)
+	go simpsonRule(-math.Pi, math.Pi, 10, number_of_terms, point, ch)
 	half_step = <-ch
 
 	for n := 5; math.Abs(step-half_step) >= eps; n++ {
 		go simpsonRule(-math.Pi, math.Pi, n, number_of_terms, point, ch)
 		step = <-ch
-		fmt.Println(step)
+
 		go simpsonRule(-math.Pi, math.Pi, 2*n, number_of_terms, point, ch)
 		half_step = <-ch
-		fmt.Println(half_step)
+
 	}
+	fmt.Println(step)
 	simpResult := step
 
 	return simpResult
@@ -84,17 +85,20 @@ func RungeTrap(point float64, number_of_terms int) float64 {
 	half_step := float64(0)
 	ch := make(chan float64, 2)
 
-	go trapezoid_rule(-math.Pi, math.Pi, 5, number_of_terms, 0, ch)
+	go trapezoid_rule(-math.Pi, math.Pi, 2, number_of_terms, point, ch)
 	step = <-ch
-	go trapezoid_rule(-math.Pi, math.Pi, 5, number_of_terms, 0, ch)
+	fmt.Println(step)
+	go trapezoid_rule(-math.Pi, math.Pi, 4, number_of_terms, point, ch)
 	half_step = <-ch
+
 	for n := 5; math.Abs(step-half_step) >= eps; n++ {
 		go trapezoid_rule(-math.Pi, math.Pi, n, number_of_terms, point, ch)
 		step = <-ch
-		fmt.Println(step)
+
 		go trapezoid_rule(-math.Pi, math.Pi, 2*n, number_of_terms, point, ch)
 		half_step = <-ch
-		fmt.Println(half_step)
+
+		fmt.Println("step:", step, half_step, "parameter:", n, number_of_terms, point)
 	}
 
 	trapResult := step
